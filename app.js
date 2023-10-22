2781b85726297f3a5345d05d24e6c495c356d3cd
tnazarov3 <148562555+tnazarov3@users.noreply.github.com>
2023-10-22 19:37:48 +0400

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let item = "";
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 
-tg.MainButton.setText("Вывести информацию по выбранному парку");
+tg.MainButton.setText("Вывести информацию по парку");
 
 btn1.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
